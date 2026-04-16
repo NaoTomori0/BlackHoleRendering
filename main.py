62229170b145676f4d413f47631666408ffc52ae
NaoTomori0 <kyongfam0@gmail.com>
2026-04-16 18:46:42 +0300

    update

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def compute_full_frame(
                     th = -py / (vy + 1e-9)
                     hx, hz = px + vx * th, pz + vz * th
                     r2_d = hx * hx + hz * hz
-                    if 6.0 < r2_d < 100.0:
+                    if 3.5 < r2_d < 100.0:
                         rv = np.sqrt(r2_d)
                         ang = np.arctan2(hz, hx)
                         wave = (
@@ -55,9 +55,11 @@ def compute_full_frame(
                             + 0.4 * np.sin(2.5 * rv + ang * 3.0)
                         ) / 1.4
 
-                        radial_boost = 15.0 / (rv**1.3 + 1.0)
+                        radial_boost = 120.0 / (rv**5 + 1.0)
+
                         dop = 1.0 + 0.6 * np.sin(ang)
-                        val = (1 + wave) * dop * radial_boost * np.exp(-0.05 * rv)
+                        # val = (1 + wave) * dop * radial_boost * np.exp(-0.05 * rv) * 2.3
+                        val = (1 + wave) * dop * radial_boost * np.exp(-0.02 * rv) * 3.2
 
                         dist_hit = np.sqrt(
                             (hx - cx) ** 2 + (py - cy) ** 2 + (hz - cz) ** 2
@@ -65,14 +67,16 @@ def compute_full_frame(
                         dist_to_hole = np.sqrt(cx**2 + cy**2 + cz**2)
 
                         if dist_hit < dist_to_hole:
-                            inst += val * 1.6
+                            inst += val * 2
                             acc_light += 0.75
                             if acc_light >= 1.0:
                                 break
                         else:
-                            inst += val * 0.75
+                            val *= 0.3
+                            inst += val * 0.43
 
-                a = -1.5 * RS / (r2 * r + 1e-6)
+                a = -0.95 * RS / (r2 * r + 1e-6)
+
                 vx += px * a * DT
                 vy += py * a * DT
                 vz += pz * a * DT
@@ -85,11 +89,13 @@ def compute_full_frame(
             if hit_hole:
                 img[i, j] = 0.0
             else:
-                ps = np.exp(-20 * (min_r - 1.01) ** 2) * 3.0
-                glow = np.exp(-1.0 * (min_r - 1.2) ** 2) * 0.4
-                b = inst * 0.05 + ps * 0.8
-                g = inst * 0.7 + ps * 0.6 + glow * 0.2
-                r_c = inst * 2.1 + ps + glow
+                ps = np.exp(-2000 * (min_r - 1.01) ** 2) * 9.0
+
+                glow = np.exp(-1.2 * (min_r - 1.2) ** 2) * 0.6
+
+                r_c = inst * 5.0 + ps * 3.0 + glow * 1.5
+                g = inst * 3.5 + ps * 3.0 + glow * 0.8
+                b = inst * 2.2 + ps * 3.0 + glow * 0.4
 
                 img[i, j, 0] = 1.0 - np.exp(-b)
                 img[i, j, 1] = 1.0 - np.exp(-g)
@@ -98,7 +104,7 @@ def compute_full_frame(
 
 
 def render():
-    RES = 700  # 500
+    RES = 300  # 500
     STEPS = 700
     DT = 0.12
 
@@ -116,12 +122,29 @@ def render():
         t = (frame_idx / 300) * 2 * np.pi
 
         dist = 45.0
-        cx, cy, cz = np.cos(t) * dist, np.sin(t * 0.2) * -15.0, np.sin(t) * dist
+        vertical_angle = -np.radians(15) * np.sin(t * 0.2)
+
+        horizontal_dist = dist * np.cos(vertical_angle)
+        cx, cy, cz = (
+            np.cos(t) * horizontal_dist,
+            dist * np.sin(vertical_angle),
+            np.sin(t) * horizontal_dist,
+        )
+
+        # cx, cy, cz = np.cos(t) * dist, np.sin(t * 0.2) * -15.0, np.sin(t) * dist
+
         cp = np.array([cx, cy, cz])
         fw = -cp / np.linalg.norm(cp)
-        rt = np.array([-np.sin(t), 0, np.cos(t)])
-        up = np.cross(rt, fw)
-        up /= np.linalg.norm(up)
+
+        rt_base = np.array([-np.sin(t), 0, np.cos(t)])
+
+        up_base = np.cross(rt_base, fw)
+        up_base /= np.linalg.norm(up_base)
+
+        tilt_angle = (np.pi / 12) * np.sin(t * 0.3)
+
+        rt = rt_base * np.cos(tilt_angle) + np.cross(fw, rt_base) * np.sin(tilt_angle)
+        up = up_base * np.cos(tilt_angle) + np.cross(fw, up_base) * np.sin(tilt_angle)
 
         frame = compute_full_frame(
             RES,
@@ -144,6 +167,7 @@ def render():
             up[1],
             up[2],
         )
+
         cv2.imshow("Black Hole", frame)
 
         curr_fps = 1.0 / (time.time() - start_t)
